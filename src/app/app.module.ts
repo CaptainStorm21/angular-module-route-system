e30fa9994d1c0e9308e823216929d806b22678a4
Captain Storm <CaptainStorm21@users.noreply.github.com>
2021-06-28 22:52:00 -0700

    importing exporting modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ElementsModule } from './elements/elements.module';
 
 @NgModule({
   declarations: [
@@ -11,9 +12,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ElementsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+/*
+* line 13 to 16 we are importing the entire module directory as an import to allow components use it
+*/
--- src/app/elements/elements.module.ts
+++ src/app/elements/elements.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ElementsRoutingModule } from './elements-routing.module';
+import { ElementsHomeComponent } from './elements-home/elements-home.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ElementsHomeComponent],
   imports: [
     CommonModule,
     ElementsRoutingModule
+  ],
+  exports: [
+    ElementsHomeComponent
   ]
 })
 export class ElementsModule { }
+
+/*
+line 14 to 16. we are telling inner component ElementsHomeComponent
+that it is allowed to be imported to the outside world
+*/
